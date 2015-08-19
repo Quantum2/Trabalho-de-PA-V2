bdc352ad96af388a496b4a85b5e74b583651d435
Quantum2 <rafaelfrancisco6@gmail.com>
2015-08-19 15:56:34 +0100

    Continuação da implementação da classe jogo

---
+++ src/MW/Misc/Nave.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MW.Misc;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Nave {
+    
+}
--- src/logicaJogo/Jogo.java
+++ src/logicaJogo/Jogo.java
@@ -6,27 +6,38 @@
 package logicaJogo;
 
 import MW.Mapa.Mapa;
+import MW.Mapa.PPirata;
+import MW.Mapa.Planeta;
+import MW.Mapa.Vazio;
+import MW.Mapa.VazioNotDiscovered;
+import MW.Mapa.Wormhole;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Observable;
+import java.util.Random;
 
 /**
  *
  * @author Rafael
  */
 public class Jogo extends Observable implements Serializable {
+
     Estado estado;
     Jogador jogador;
-    
+
     int linhas = 7;
     int colunas = 9;
-    
-    public Mapa [][] mapa=new Mapa[linhas][colunas]; 
-    ArrayList<String> dado=new ArrayList<>(Arrays.asList("Branco","Branco","Preto","Azul","Vermelho","Amarelo"));
+
+    public Mapa[][] mapa = new Mapa[linhas][colunas];
+    ArrayList<String> dado = new ArrayList<>(Arrays.asList("Branco", "Branco", "Preto", "Azul", "Vermelho", "Amarelo"));
+
+    ArrayList<Integer> XTrans = new ArrayList();
+    ArrayList<Integer> YTrans = new ArrayList();
 
     public Jogo() {
         estado = new EsperaInicio(this);
+        jogador = new Jogador();
     }
 
     public Estado getEstado() {
@@ -36,6 +47,89 @@ public class Jogo extends Observable implements Serializable {
     public void setEstado(Estado estado) {
         this.estado = estado;
     }
-    
+
+    int getRandomCusto() {
+        return new Random().nextInt(4);
+    }
+
+    int getRandomDado() {
+        return new Random().nextInt(6);
+    }
+
+    int getRandomM(ArrayList<Integer> n) {
+        return new Random().nextInt(n.size());
+    }
+
+    public void fazerMapa() {
+        ArrayList<Integer> x = new ArrayList();
+        x.addAll(Arrays.asList(0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 8, 8));
+        ArrayList<Integer> y = new ArrayList();
+        y.addAll(Arrays.asList(5, 4, 4, 3, 4, 3, 2, 3, 2, 1, 2, 3, 4, 3, 4, 5, 2, 3, 4, 2, 3, 2, 1));
+
+        Wormhole aux1 = new Wormhole(0, 6);
+        aux1.setCheck(Boolean.TRUE);
+        aux1.setNave(Boolean.TRUE);
+
+        Wormhole aux2 = new Wormhole(8, 0);
+        aux2.setCheck(Boolean.TRUE);
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                VazioNotDiscovered e = new VazioNotDiscovered(j, i);
+                mapa[i][j] = e;
+            }
+        }
+
+        XTrans.add(0);
+        YTrans.add(6);
+        XTrans.add(8);
+        YTrans.add(0);
+
+        mapa[6][0] = aux1;
+        mapa[0][8] = aux2;
+
+        for (int i = 0; i < 2; i++) {
+            int pos = getRandomM(x);
+            mapa[y.get(pos)][x.get(pos)] = new Wormhole(y.get(pos), x.get(pos));
+            XTrans.add(x.get(pos));
+            YTrans.add(y.get(pos));
+            x.remove(pos);
+            y.remove(pos);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            int conta = 0;
+            int pos = getRandomM(x);
+            mapa[y.get(pos)][x.get(pos)] = new Planeta(y.get(pos), x.get(pos));
+            while (conta < 4) {
+                mapa[y.get(pos)][x.get(pos)].setCusto(getRandomCusto() + 1, conta);
+                conta++;
+            }
+            x.remove(pos);
+            y.remove(pos);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int conta = 0;
+            int pos = getRandomM(x);
+            mapa[y.get(pos)][x.get(pos)] = new PPirata(y.get(pos), x.get(pos));
+            while (conta < 4) {
+                mapa[y.get(pos)][x.get(pos)].setCusto(getRandomCusto() + 1, conta);
+                conta++;
+            }
+            x.remove(pos);
+            y.remove(pos);
+        }
+
+        for (int i = 0; i < 12; i++) {
+            int pos = getRandomM(x);
+            mapa[y.get(pos)][x.get(pos)] = new Vazio(y.get(pos), x.get(pos));
+            x.remove(pos);
+            y.remove(pos);
+        }
+
+        setChanged();
+        notifyObservers();
+    }
     
 }
