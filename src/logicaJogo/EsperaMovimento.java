8fa65a5474bc72e0f93df761421c6ce52c9f51d2
Quantum2 <rafaelfrancisco6@gmail.com>
2015-08-21 15:23:56 +0100

    Mais dois estados completos

--- src/logicaJogo/EsperaMovimento.java
+++ src/logicaJogo/EsperaMovimento.java
@@ -6,6 +6,7 @@
 package logicaJogo;
 
 import java.io.Serializable;
+import static java.lang.System.exit;
 
 /**
  *
@@ -19,42 +20,138 @@ public class EsperaMovimento extends Estado implements Serializable {
 
     @Override
     public Estado Upgrade(int escolha) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public Estado Compra(int escolhacompra) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public Estado Movimento(int opcao, int conta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int x,y;
+        x=J.jogador.getNave().getX();
+        y=J.jogador.getNave().getY();
+        
+        if(opcao==1)
+        {
+            if(y+1<7 && J.mapa[y+1][x].getCheck()==true && !" ".equals(J.mapa[y+1][x].getTexto()))
+            {
+                J.jogador.getNave().setY(y+1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==2)
+        {
+            if(y-1>-1 && J.mapa[y-1][x].getCheck()==true && !" ".equals(J.mapa[y-1][x].getTexto()))
+            {
+                J.jogador.getNave().setY(y-1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==3)
+        {
+            if(x+1<9 && J.mapa[y][x+1].getCheck()==true && !" ".equals(J.mapa[y][x+1].getTexto()))
+            {
+                J.jogador.getNave().setX(x+1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==4)
+        {
+            if(x-1>-1 && J.mapa[y][x-1].getCheck()==true &&!" ".equals(J.mapa[y][x-1].getTexto()))
+            {
+                J.jogador.getNave().setX(x-1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==5)
+        {
+            if(y+1<7 && x+1<9 && J.mapa[y+1][x+1].getCheck()==true && !" ".equals(J.mapa[y+1][x+1].getTexto()))
+            {
+                J.jogador.getNave().setY(y+1);
+                J.jogador.getNave().setX(x+1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==6)
+        {
+            if(y+1<7 && x-1>-1 && J.mapa[y+1][x-1].getCheck()==true && !" ".equals(J.mapa[y+1][x-1].getTexto()))
+            {
+                J.jogador.getNave().setY(y+1);
+                J.jogador.getNave().setX(x-1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==7)
+        {
+            if(y-1>-1 && x+1<9 && J.mapa[y-1][x+1].getCheck()==true && !" ".equals(J.mapa[y-1][x+1].getTexto()))
+            {
+                J.jogador.getNave().setY(y-1);
+                J.jogador.getNave().setX(x+1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        if(opcao==8)
+        {
+            if(y-1>-1 && x-1>-1 && J.mapa[y-1][x-1].getCheck()==true && !" ".equals(J.mapa[y-1][x-1].getTexto()))
+            {
+                J.jogador.getNave().setY(y-1);
+                J.jogador.getNave().setX(x-1);
+                J.jogador.setMoney(J.jogador.getMoney()-1);
+            }else{
+                opcao=0;
+            }
+        }
+        
+        if(opcao==9)
+        {
+                J.jogador.getNave().setX(J.XTrans.get(conta));
+                J.jogador.getNave().setY(J.YTrans.get(conta));
+            }else{
+                opcao=0;
+            }
+        return new EsperaMovimento(this.J);
     }
 
     @Override
     public Estado Vende(int escolhavenda) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public Estado fimEstado() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new EsperaVenda(this.J);
     }
 
     @Override
     public Estado Recomeca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public void fimJogo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        exit(0);
     }
 
     @Override
     public int ganha() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return -1;
     }
     
 }
--- src/logicaJogo/EsperaVenda.java
+++ src/logicaJogo/EsperaVenda.java
@@ -7,6 +7,7 @@ package logicaJogo;
 
 import MW.Recs.RecBranco;
 import java.io.Serializable;
+import static java.lang.System.exit;
 
 /**
  *
@@ -19,144 +20,131 @@ public class EsperaVenda extends Estado implements Serializable {
     }
 
     @Override
-    public Estado Vende(int escolhavenda)        
-    {
-        int x=J.jogador.getNave().getX();
-        int y=J.jogador.getNave().getY();
-        
-        if("P".equals(J.mapa[y][x].getTexto()))
-        {
-                if(!"Branco".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()) && !"Cinzento".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                {
-                    if(!"Branco".equals(J.mapa[y][x].getRecursos().get(0).getNome()))
-                    {
-                        if("Azul".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                        {
-                            J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(0));
-                        }else{
-                            if("Amarelo".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                            {
-                                J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(1));
-                            }else{
-                                if("Vermelho".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                {
-                                    J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(2));
-                                }else{
-                                    if("Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                    {
-                                        J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(3));
-                                    }
+    public Estado Vende(int escolhavenda) {
+        int x = J.jogador.getNave().getX();
+        int y = J.jogador.getNave().getY();
+
+        if ("P".equals(J.mapa[y][x].getTexto())) {
+            if (!"Branco".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()) && !"Cinzento".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                if (!"Branco".equals(J.mapa[y][x].getRecursos().get(0).getNome())) {
+                    if ("Azul".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                        J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(0));
+                    } else {
+                        if ("Amarelo".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                            J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(1));
+                        } else {
+                            if ("Vermelho".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                                J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(2));
+                            } else {
+                                if ("Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                                    J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(3));
                                 }
                             }
-                        }
-                        if(!"Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                        {
-                            J.mapa[y][x].getRecursos().set(0,J.jogador.getNave().getCargo().get(escolhavenda));
-                            J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
-                        }else{
-                            J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
-                        }
-                    }else{
-                        if(!"Branco".equals(J.mapa[y][x].getRecursos().get(1).getNome()))
-                        {
-                            if("Azul".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                            {
-                                J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(0));
-                            }else{
-                                if("Amarelo".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                {
-                                    J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(1));
-                                }else{
-                                    if("Vermelho".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                    {
-                                        J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(2));
-                                    }else{
-                                        if("Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                        {
-                                            J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(3));
-                                        }
-                                    }
-                                }
-                            }
-                        if(!"Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                        {
-                            J.mapa[y][x].getRecursos().set(0,J.jogador.getNave().getCargo().get(escolhavenda));
-                            J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
-                        }else{
-                            J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
-                        }
-                        }else{
-                            System.out.println("Operacao Impossivel");
                         }
                     }
-                }else{
-                    System.out.println("Operacao Impossivel");
-                    } 
-        }
-        
-        if("X".equals(J.mapa[y][x].getTexto()))
-        {
-                if(!"Branco".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()) && !"Cinzento".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()) )
-                {
-                    if("Azul".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                        {
-                            J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(0));
-                        }else{
-                            if("Amarelo".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                            {
-                                J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(1));
-                            }else{
-                                if("Vermelho".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                {
-                                    J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(2));
-                                }else{
-                                    if("Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()))
-                                    {
-                                        J.jogador.setMoney(J.jogador.getMoney()+J.mapa[y][x].getCusto().get(3));
+                    if (!"Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                        J.mapa[y][x].getRecursos().set(0, J.jogador.getNave().getCargo().get(escolhavenda));
+                        J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
+                    } else {
+                        J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
+                    }
+                } else {
+                    if (!"Branco".equals(J.mapa[y][x].getRecursos().get(1).getNome())) {
+                        if ("Azul".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                            J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(0));
+                        } else {
+                            if ("Amarelo".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                                J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(1));
+                            } else {
+                                if ("Vermelho".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                                    J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(2));
+                                } else {
+                                    if ("Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                                        J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(3));
                                     }
                                 }
                             }
                         }
-                J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());               
+                        if (!"Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                            J.mapa[y][x].getRecursos().set(0, J.jogador.getNave().getCargo().get(escolhavenda));
+                            J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
+                        } else {
+                            J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
+                        }
+                    } else {
+                        System.out.println("Operacao Impossivel");
+                    }
                 }
+            } else {
+                System.out.println("Operacao Impossivel");
             }
-        
+        }
+
+        if ("X".equals(J.mapa[y][x].getTexto())) {
+            if (!"Branco".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome()) && !"Cinzento".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                if ("Azul".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                    J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(0));
+                } else {
+                    if ("Amarelo".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                        J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(1));
+                    } else {
+                        if ("Vermelho".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                            J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(2));
+                        } else {
+                            if ("Preto".equals(J.jogador.getNave().getCargo().get(escolhavenda).getNome())) {
+                                J.jogador.setMoney(J.jogador.getMoney() + J.mapa[y][x].getCusto().get(3));
+                            }
+                        }
+                    }
+                }
+                J.jogador.getNave().getCargo().set(escolhavenda, new RecBranco());
+            }
+        }
+
         return new EsperaVenda(this.J);
     }
-    
+
     @Override
     public Estado Upgrade(int escolha) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (escolha == 1) {
+            J.jogador.getNave().setPower(5);
+            J.jogador.setMoney(J.jogador.getMoney() - 4);
+        } else {
+            if (escolha == 2 && J.jogador.getMoney() - 5 > 0) {
+                J.jogador.getNave().getCargo().set(2, new RecBranco());
+            }
+        }
+        return new EsperaVenda(this.J);
     }
 
     @Override
     public Estado Compra(int escolhacompra) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public Estado Movimento(int opcao, int conta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public Estado fimEstado() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new EsperaCompra(this.J);
     }
 
     @Override
     public Estado Recomeca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this;
     }
 
     @Override
     public void fimJogo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        exit(0);
     }
 
     @Override
     public int ganha() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return -1;
     }
 
 }
